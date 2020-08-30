156ac41d4e9cf4d288d0f663b72bbffbc09fa24c
Alla Kuzminova <alchik@gmail.com>
2020-08-30 17:21:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,13 @@
 def turn_count(board)
-
+  count = 0
+  board.each do  |item|
+    if !(item == " " || item == "" )
+      count += 1
+    end
+  end
+  return count
 end
 
 def current_player(board)
+
 end
